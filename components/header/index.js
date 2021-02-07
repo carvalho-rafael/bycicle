b9d40b2c9032ce2ffc9fd26d03e032d284853743
Rafael Carvalho <rafaelvfcarvalho@gmail.com>
2021-02-06 21:34:45 -0300

    create header component

---
+++ components/header/index.js
@@ -0,0 +1,14 @@
+import Style from './style.module.scss'
+import Image from 'next/image'
+export default function Header() {
+    return (
+        <div className={Style.container}>
+{/*             <Image
+                src="/images/profile.jpg" // Route of the image file
+                height={844} // Desired size with correct aspect ratio
+                width={600} // Desired size with correct aspect ratio
+                alt="Your Name"
+            />
+ */}        </div>
+    )
+}
