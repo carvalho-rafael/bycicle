6cd3170d30bc27bdd4ce635f5ee31c0db1fe3130
Rafael Carvalho <rafaelvfcarvalho@gmail.com>
2021-03-12 14:31:18 -0300

    send email from contact form

--- components/form/index.js
+++ components/form/index.js
@@ -1,10 +1,28 @@
+import { useState } from 'react';
 import Style from './style.module.scss'
 
 export default function Form() {
-    function handleSubmit(event) {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [age, setAge] = useState(20);
+    const [message, setMessage] = useState('');
+
+    async function handleSubmit(event) {
         event.preventDefault();
-        console.log("sent");
+        
+        const body = { name, email, age, message }
+        console.log(body)
+        const result = await fetch('/api/contact', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(body),
+        })
+
+        if (result) {
+            console.log(result);
+        }
     }
+
     return (
         <div className={Style.formWrapper}>
             <div className={Style.formContainer}>
@@ -15,19 +33,19 @@ export default function Form() {
                 <form onSubmit={handleSubmit}>
                     <div className={Style.formGroup}>
                         <label htmlFor='name'>Name</label>
-                        <input id='name' type='text' ></input>
+                        <input onChange={(e) => setName(e.target.value)} id='name' type='text' />
                     </div>
                     <div className={Style.formGroup}>
                         <label htmlFor='email'>Email</label>
-                        <input id='email' type='email' ></input>
+                        <input onChange={(e) => setEmail(e.target.value)} id='email' type='email' />
                     </div>
                     <div className={Style.formGroup}>
                         <label htmlFor='idade'>Your age</label>
-                        <input id='idade' type='number' ></input>
+                        <input onChange={(e) => setAge(e.target.value)} defaultValue={age} id='idade' type='number' />
                     </div>
                     <div className={Style.formGroup}>
                         <label htmlFor='message'>Message</label>
-                        <textarea id='message' type='textarea' rows='8' ></textarea>
+                        <textarea onChange={(e) => setMessage(e.target.value)} id='message' type='textarea' rows='8' />
                     </div>
                     <div className={Style.formGroup}>
                         <button id='submit' type='submit' >
---
+++ pages/api/contact.js
@@ -0,0 +1,32 @@
+import nodemailer from 'nodemailer';
+
+export default async function handle(req, res) {
+    const { name, email, age, message } = req.body;
+
+    let transporter = nodemailer.createTransport({
+        host: "smtp.zoho.com",
+        port: 465,
+        secure: true,
+        auth: {
+            user: process.env.ZOHO_USER,
+            pass: process.env.ZOHO_PASS,
+        },
+    });
+
+    try {
+        let info = await transporter.sendMail({
+            from: 'rafael@carvalhorafael.dev.br',
+            to: 'rafaelvfcarvalho@gmail.com',
+            subject: 'Bycicle Contact',
+            html:
+                `<p><b>${name}, ${age} anos</b></p>\
+                <p>Email: ${email}</p>\
+                <p>Message: ${message}</p>`,
+        });
+    } catch (error) {
+        console.error(error);
+        res.json({ 'message': 'erro' });
+    }
+
+    res.json({ 'message': 'ok' });
+}
